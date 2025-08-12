1fd05fe96bc81a971a7510b52c73c8dd883fd022
leedongha1998 <leedongha98@gmail.com>
2025-08-12 19:22:14 +0900

    first commit

---
+++ src/test/java/com/example/order_api_tuning/OrderApiTuningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.order_api_tuning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApiTuningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
